d70ebb46bee35b39e396e53be2e919b33b0278c7
renatofrancisco <renatofrancisco.s@gmail.com>
2021-05-19 22:13:30 -0300

    Conta Poupança

---
+++ contas/contaPoupanca.go
@@ -0,0 +1,44 @@
+package contas
+
+import "banco/clientes"
+
+type ContaPoupanca struct {
+	Titular       clientes.Cliente
+	NumeroAgencia int
+	NumeroConta   int
+	Operacao      int
+	saldo         float64
+}
+
+func (c *ContaPoupanca) Sacar(valorDoSaque float64) string {
+	podeSacar := valorDoSaque > 0 && valorDoSaque <= c.saldo
+	if podeSacar {
+		c.saldo -= valorDoSaque
+		return "Sque realizado com sucesso"
+	} else {
+		return "saldo insuficiente"
+	}
+}
+
+func (c *ContaPoupanca) Depositar(valorDoDeposito float64) (string, float64) {
+	if valorDoDeposito > 0 {
+		c.saldo += valorDoDeposito
+		return "Depósito realizado com sucesso", c.saldo
+	} else {
+		return "Valor do depósito menor que zero", c.saldo
+	}
+}
+
+func (c *ContaPoupanca) Transferir(valorDaTransferencia float64, contaDestino *ContaCorrente) bool {
+	if valorDaTransferencia < c.saldo && valorDaTransferencia > 0 {
+		c.saldo -= valorDaTransferencia
+		contaDestino.Depositar(valorDaTransferencia)
+		return true
+	} else {
+		return false
+	}
+}
+
+func (c *ContaPoupanca) ObterSaldo() float64 {
+	return c.saldo
+}
