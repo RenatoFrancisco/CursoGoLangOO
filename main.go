11e25ff593fc5f3e4115b14da82b1decc31cdfce
renatofrancisco <renatofrancisco.s@gmail.com>
2021-05-19 22:05:08 -0300

    Alterando a visbilidade

--- main.go
+++ main.go
@@ -7,12 +7,12 @@ import (
 )
 
 func main() {
-	contaDoRenato := contas.ContaCorrente{Titular: clientes.Cliente{Nome: "Renato"}, NumeroAgencia: 589, NumeroConta: 123456, Saldo: 125.5}
-	contaDoRenato2 := contas.ContaCorrente{Titular: clientes.Cliente{Nome: "Renato"}, NumeroAgencia: 589, NumeroConta: 123456, Saldo: 125.5}
+	contaDoRenato := contas.ContaCorrente{Titular: clientes.Cliente{Nome: "Renato"}, NumeroAgencia: 589, NumeroConta: 123456}
+	contaDoRenato2 := contas.ContaCorrente{Titular: clientes.Cliente{Nome: "Renato"}, NumeroAgencia: 589, NumeroConta: 123456}
 	fmt.Println(contaDoRenato == contaDoRenato2) //true
 
-	contaDaBruna := contas.ContaCorrente{clientes.Cliente{Nome: "Bruna"}, 222, 112223, 200}
-	contaDoGuilherme := contas.ContaCorrente{Titular: clientes.Cliente{Nome: "Guilherme"}, Saldo: 150.5}
+	contaDaBruna := contas.ContaCorrente{Titular: clientes.Cliente{Nome: "Bruna"}, NumeroAgencia: 222, NumeroConta: 112223}
+	contaDoGuilherme := contas.ContaCorrente{Titular: clientes.Cliente{Nome: "Guilherme"}}
 
 	fmt.Println(contaDoRenato)
 	fmt.Println(contaDaBruna)
@@ -21,12 +21,14 @@ func main() {
 	var contaDaCris *contas.ContaCorrente
 	contaDaCris = new(contas.ContaCorrente)
 	contaDaCris.Titular.Nome = "Cris"
-	contaDaCris.Saldo = 500
+	// contaDaCris.Saldo = 500
+	contaDaCris.Depositar(500)
 
 	var contaDaCris2 *contas.ContaCorrente
 	contaDaCris2 = new(contas.ContaCorrente)
 	contaDaCris2.Titular.Nome = "Cris"
-	contaDaCris2.Saldo = 500
+	// contaDaCris2.Saldo = 500
+	contaDaCris2.Depositar(500)
 	fmt.Println(contaDaCris == contaDaCris2)   //false
 	fmt.Println(*contaDaCris == *contaDaCris2) //true
 
@@ -34,15 +36,16 @@ func main() {
 
 	contaDaSilvia := contas.ContaCorrente{}
 	contaDaSilvia.Titular.Nome = "Silvia"
-	contaDaSilvia.Saldo = 500.
+	// contaDaSilvia.Saldo = 500.
+	contaDaSilvia.Depositar(500)
 
-	fmt.Println(contaDaSilvia.Saldo)
+	fmt.Println(contaDaSilvia.ObterSaldo())
 	fmt.Println(contaDaSilvia.Sacar(200))
-	fmt.Println(contaDaSilvia.Saldo)
+	fmt.Println(contaDaSilvia.ObterSaldo())
 
 	status, valor := contaDaSilvia.Depositar(1000)
 	fmt.Println(status, valor)
 
 	resultado := contaDaSilvia.Transferir(100, contaDaCris)
-	fmt.Print(resultado, contaDaSilvia.Saldo, contaDaCris.Saldo)
+	fmt.Print(resultado, contaDaSilvia.ObterSaldo(), contaDaCris.ObterSaldo())
 }
